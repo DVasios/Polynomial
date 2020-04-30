ef42a316286f1872f8a89d64d391f7a806c55a05
Dimitris Vasios <dimitrisvasios99@gmail.com>
2020-05-01 01:00:48 +0300

    better version

--- polynomial.cpp
+++ polynomial.cpp
@@ -2,6 +2,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+
 class Polynomial {
 protected:
     class Term {
@@ -15,29 +16,26 @@ protected:
     };
 
 public:
-    Polynomial() : front(nullptr), rear(nullptr) {}
-    Polynomial(const Polynomial &p) : front(nullptr), rear(nullptr) {
+    Polynomial() : front(nullptr) {}
+    Polynomial(const Polynomial &p) : front(nullptr) {
         Term *temp = p.front;
-        front = rear = new Term(temp->exponent, temp->coefficient, temp->next);
-        temp = temp->next;
+        front = nullptr;
         while(temp != nullptr) {
-            rear->next = new Term(temp->exponent, temp->coefficient, nullptr);
-            rear = rear->next;
+            this->addTerm(temp->exponent, temp->coefficient);
             temp = temp->next;
         }
     }
 
     ~Polynomial() { 
-        front = rear = nullptr;
+        front = nullptr;
     }
 
     Polynomial & operator = (const Polynomial &p) {
         delete front;
-        delete rear;
-        Term *temp;
-        front = rear = temp = new Term(p.front->exponent, p.front->coefficient, p.front->next);
-        while(temp->next != nullptr) {
-            rear->next = new Term(temp->exponent, temp->coefficient, nullptr);
+        Term *temp = p.front;
+        front = nullptr;
+        while(temp != nullptr) {
+            this->addTerm(temp->exponent, temp->coefficient);
             temp = temp->next;
         }
         return *this;
@@ -45,7 +43,7 @@ public:
 
     void addTerm(int expon, int coeff) {
         if (front == nullptr ) {
-            front = rear = new Term(expon, coeff, nullptr);
+            front = new Term(expon, coeff, nullptr);
             
         }
         else {
@@ -57,8 +55,8 @@ public:
                 if(front->coefficient == 0 && front->next != nullptr) {
                     front = front->next;
                 }
-                else if (front->coefficient == 0 && front->next != nullptr) {
-                    front = rear = nullptr;
+                else if (front->coefficient == 0 && front->next == nullptr) {
+                    front = nullptr;
                 }
             }
             else {
@@ -77,7 +75,6 @@ public:
                             }
                             else if (temp->next->coefficient == 0 && temp->next->next == nullptr) {
                                 temp->next = nullptr;
-                                rear = temp;
                             }
                             k = false;
                         }
@@ -89,7 +86,6 @@ public:
                     }
                     else {
                         temp->next =  new Term(expon, coeff, nullptr);
-                        rear = temp->next;
                         k = false;
                     }
                 }
@@ -112,19 +108,42 @@ public:
         Polynomial pol;
         Term *temp_p = p.front;
         Term *temp_q = q.front;
-         
-        while (temp_p != nullptr || temp_q != nullptr) { 
-            if ((temp_p != nullptr && temp_q == nullptr) || (temp_p->exponent > temp_q->exponent)) {
-                pol.addTerm(temp_p->exponent, temp_p->coefficient);
-                temp_p = temp_p->next;
-            }
-            else if (temp_p->exponent == temp_q->exponent) {
-                pol.addTerm(temp_p->exponent, temp_p->coefficient + temp_q->coefficient);                temp_p = temp_p->next;
-                temp_q = temp_q->next;
-            }
-            else if ((temp_p == nullptr && temp_q != nullptr) || (temp_p->exponent < temp_q->exponent)) {
-                pol.addTerm(temp_q->exponent, temp_q->coefficient);               
-                temp_q = temp_q->next;
+        pol.front = nullptr;
+        if( p.front == nullptr) {
+            pol = q;
+        }
+        else if ( q.front == nullptr) {
+            pol = p;
+        }
+        else {
+            while (temp_p != nullptr || temp_q != nullptr) { 
+                if ((temp_p != nullptr && temp_q == nullptr)) {
+                    pol.addTerm(temp_p->exponent, temp_p->coefficient);
+                    temp_p = temp_p->next;
+                }
+                else if ((temp_p == nullptr && temp_q != nullptr)) {
+                    pol.addTerm(temp_q->exponent, temp_q->coefficient);               
+                    temp_q = temp_q->next;
+                }
+                else if ((temp_p->exponent > temp_q->exponent)) {
+                    pol.addTerm(temp_p->exponent, temp_p->coefficient);
+                    temp_p = temp_p->next;
+                }
+                else if ((temp_p->exponent < temp_q->exponent)) {
+                    pol.addTerm(temp_q->exponent, temp_q->coefficient);               
+                    temp_q = temp_q->next;
+                }
+                else if (temp_p->exponent == temp_q->exponent) {
+                    if (temp_p->coefficient + temp_q->coefficient == 0) {
+                        temp_p = temp_p->next;
+                        temp_q = temp_q->next;
+                    }
+                    else {
+                        pol.addTerm(temp_p->exponent, temp_p->coefficient + temp_q->coefficient);                
+                        temp_p = temp_p->next;
+                        temp_q = temp_q->next;
+                    }
+                }
             }
         }
         return pol;
@@ -135,57 +154,62 @@ public:
         Polynomial pol;
         Term *temp_p = p.front; 
         Term *temp_q = q.front;
-        
-        while (temp_p != nullptr) {
-            while(temp_q != nullptr) {
-                pol.addTerm(temp_p->exponent + temp_q->exponent, temp_p->coefficient * temp_q->coefficient);
-                temp_q = temp_q->next;
+        pol.front == nullptr;
+        if ( p.front != nullptr || q.front != nullptr) {
+            while (temp_p != nullptr) {
+                while(temp_q != nullptr) {
+                    pol.addTerm(temp_p->exponent + temp_q->exponent, temp_p->coefficient * temp_q->coefficient);
+                    temp_q = temp_q->next;
+                }
+                temp_q = q.front;
+                temp_p = temp_p ->next;
             }
-            temp_q = q.front;
-            temp_p = temp_p ->next;
         }
         return pol;
     }
 
     friend ostream & operator << (ostream &out, const Polynomial &p) {
         Term *temp = p.front;
-        while (temp != nullptr) {
-            if (temp->exponent != p.front->exponent && temp->coefficient > 0) {
-                out << " + ";
-            } 
-            else if (temp->coefficient < 0) {
-                out << " - ";
-            }  
-
-            if (abs(temp->coefficient) == 1) {
-                if (abs(temp->exponent) == 1) {
-                    out << "x";
+        if (p.front == nullptr) cout << "0";
+        else {
+            while (temp != nullptr) {
+                if (temp->exponent != p.front->exponent) cout << " ";
+                if (temp->exponent != p.front->exponent && temp->coefficient > 0) {
+                    out << "+ ";
+                }   
+                else if (temp->coefficient < 0) {
+                    out << "- ";
+                }
+                if (abs(temp->coefficient) == 1) {
+                    if (abs(temp->exponent) == 1) {
+                        out << "x";
+                        temp = temp->next;
+                    }
+                    else if (temp->exponent == 0 ) {
+                        out << abs(temp->coefficient);
+                        temp = temp->next;
+                    }
+                    else {
+                        out << "x^" << temp->exponent;
+                        temp = temp->next;       
+                    }
+                }
+                else if (temp->coefficient == 0) {
                     temp = temp->next;
                 }
-                else if (temp->exponent == 0 ) {
-                    out << abs(temp->coefficient);
-                    temp = temp->next;
-                }
-                else {
-                    out << "x^" << temp->exponent;
-                    temp = temp->next;    
-                }
-            }
-            else if (temp->coefficient == 0) {
-                temp = temp->next;
-            }
-            else {   
-                if (temp->exponent == 0 ) {
-                    out << abs(temp->coefficient);
-                    temp = temp->next;
-                }
-                else if (temp->exponent == 1) {
-                    out << abs(temp->coefficient) << "x";
-                    temp = temp->next;
-                }
-                else {
-                    out << abs(temp->coefficient) << "x^" << temp->exponent;
-                    temp = temp->next;
+                else {   
+                    if (temp->exponent == 0 ) {
+                        out << abs(temp->coefficient);
+                        temp = temp->next;
+                    }
+                    else if (temp->exponent == 1) {
+                        out << abs(temp->coefficient) << "x";
+                        temp = temp->next;
+                    }
+                    else {
+                        out << abs(temp->coefficient) << "x^" << temp->exponent;
+                        temp = temp->next;
+                    }
                 }
             }
         }
@@ -193,100 +217,36 @@ public:
     }
 
 private:
-    Term *front, *rear;
+    Term *front;
 };
 
+
 int main () {
-    // Polynomial p;
-    // p.addTerm(1, 3);
-    // p.addTerm(2, 1);
-    // p.addTerm(0, -1);
-
-
-    // Polynomial q(p);
-    // q.addTerm(1, -3);
-    // q.addTerm(4,34);
-    // q.addTerm(1, 9);
-
-
-
-    // cout << "P(x) = " << p << endl;
-    // cout << "P(2) = " << p.evaluate(2) << endl;
-    // cout << "Q(x) = " << q << endl;
-    // cout << "Q(2) = " << q.evaluate(2) << endl;
-
-    // cout << "(P+Q)(x) = " << p+q << endl;
-    // cout << "(P*Q)(x) = " << p*q << endl;
-
     Polynomial p;
-    p.addTerm(2, 3);
-    p.addTerm(4, -1);
-    p.addTerm(2, -1);
-    p.addTerm(1, 6);
-    p.addTerm(0, 4);
-    cout << p << endl;
-    cout << p.evaluate(0) << endl;
-    cout << p.evaluate(1) << endl;
-    cout << p.evaluate(5) << endl;
-    cout << p.evaluate(10) << endl;
+    p.addTerm(6, -22);
+    p.addTerm(10, 1);
+    p.addTerm(2, 4);
+    //p.addTerm(1, 1);
+    cout << "P(X) = " << p << endl;
+    cout << "P(0) = " << p.evaluate(0) << endl;
+    cout << "P(1) = " << p.evaluate(1) << endl;
+    cout << "P(3) = " << p.evaluate(3) << endl;
+    cout << "P(-8) = " << p.evaluate(-8) << endl;
+    cout << endl;
 
-
-    Polynomial q(p);
-    q.addTerm(0, -3);
+    Polynomial q;
+    q.addTerm(1, 3);
+    q.addTerm(2, -9);
     q.addTerm(1, 5);
-    q.addTerm(2, -10);
-    q.addTerm(2, 4);
-    q.addTerm(0, 3);
-    cout << q << endl;
+    q.addTerm(0, 7);
+    cout << "Q(X) = " << q << endl;
+    cout << "Q(0) = " << q.evaluate(0) << endl;
+    cout << "Q(1) = " << q.evaluate(1) << endl;
+    cout << "Q(3) = " << q.evaluate(3) << endl;
+    cout << "Q(-8) = " << q.evaluate(-8) << endl;
+    cout << endl;
 
-    // cout << q.evaluate(0) << endl;
-    // cout << q.evaluate(1) << endl;
-    // cout << q.evaluate(5) << endl;
-    // cout << q.evaluate(10) << endl;
-
-    // Polynomial k = p+q;
-    // cout << k << endl;
-    // cout << k.evaluate(0) << endl;
-    // cout << k.evaluate(1) << endl;
-    // cout << k.evaluate(5) << endl;
-    // cout << k.evaluate(10) << endl;
-
-    // Polynomial d = p*q;
-    // cout << d << endl;
-    // cout << d.evaluate(0) << endl;
-    // cout << d.evaluate(1) << endl;
-    // cout << d.evaluate(5) << endl;
-    // cout << d.evaluate(10) << endl;
-
-    // Polynomial p;
-    // p.addTerm(1, 1);
-    // p.addTerm(0, -1);
-    // cout << p << endl;
-    // cout << p.evaluate(0) << endl;
-    // cout << p.evaluate(1) << endl;
-    // cout << p.evaluate(15) << endl;
-    // cout << p.evaluate(1500) << endl;
-
-    // Polynomial q;
-    // q.addTerm(1, 1);
-    // q.addTerm(0, 1);
-    // cout << q << endl;
-    // cout << q.evaluate(0) << endl;
-    // cout << q.evaluate(1) << endl;
-    // cout << q.evaluate(15) << endl;
-    // cout << q.evaluate(1500) << endl;
-
-    // Polynomial k = p+q;
-    // cout << k << endl;
-    // cout << k.evaluate(0) << endl;
-    // cout << k.evaluate(1) << endl;
-    // cout << k.evaluate(15) << endl;
-    // cout << k.evaluate(1500) << endl;
-
-    // Polynomial d = p*q;
-    // cout << d << endl;
-    // cout << d.evaluate(0) << endl;
-    // cout << d.evaluate(1) << endl;
-    // cout << d.evaluate(15) << endl;
-    // cout << d.evaluate(1500) << endl;
+    cout << "(P+Q)(x) = " << p+q << endl;
+    cout << "(P*Q)(x) = " << p*q << endl;
+    
 }
